eef5c9008d199f7f401e4ffd58a1be4b4e79a43f
Dongdong Zhang <45675253+DZhangCoder@users.noreply.github.com>
2019-07-03 13:00:01 -0700

    heroku

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/unit18Populater";
+mongoose.connect(MONGODB_URI); 
 
 app.get('/', function (req, res) {
   res.render('home');
